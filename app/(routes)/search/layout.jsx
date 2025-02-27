c560650b07ec1afee966956c833b47225b371fe1
SimoSpark <mohamedoukhajou@gmail.com>
2025-02-27 12:32:01 +0000

    selectcategory

--- app/(routes)/search/layout.jsx
+++ app/(routes)/search/layout.jsx
@@ -7,6 +7,7 @@ function layout({children}) {
         <div className='grid grid-cols-1 md:grid-cols-4 mt-8'>
             <div className='hidden md:block'>
               
+           
              <CategorySideBar/>
             </div>
             <div className='md:col-span-3 '>
