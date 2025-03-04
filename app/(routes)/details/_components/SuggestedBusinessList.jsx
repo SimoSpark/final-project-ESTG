6717b8c6c2fd4acb53e3a2b669c9869a8958c188
SimoSpark <mohamedoukhajou@gmail.com>
2025-03-04 17:36:42 +0000

    similar business

--- app/(routes)/details/_components/SuggestedBusinessList.jsx
+++ app/(routes)/details/_components/SuggestedBusinessList.jsx
@@ -1,17 +1,34 @@
+import GlobalApi from '@/app/_Services/GlobalApi';
 import { Button } from '@/components/ui/button'
 import { NotebookPen } from 'lucide-react'
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
 function SuggestedBusinessList({business}) {
-  return (
+  
+    const [businessList,setBusinessList]=useState([]);
+      useEffect(()=>{
+         
+          business&&getBusinessList()
+      },[business]);
+  
+      const getBusinessList=()=>{
+          GlobalApi.getBusinessByCategory(business?.category?.name)
+          .then(resp=>{
+              setBusinessList(resp?.businessLists);
+          })
+      }
+
+    return (
         <div className='pl-10'>
-       <Button className="flex gap-2 w-full">
-       <NotebookPen/>
-       Book Appointment  
-       </Button> 
-    
-    </div>
-  )
+            <Button className="flex gap-2 w-full">
+                <NotebookPen />
+                Book Appointment
+            </Button>
+            <h2 className='font-bold 
+      text-lg mt-3 mb-3
+      
+      '>Similar Business</h2>
+        </div>
+    )
 }
 
 export default SuggestedBusinessList
