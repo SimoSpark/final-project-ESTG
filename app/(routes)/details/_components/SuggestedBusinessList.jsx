32e5f86244fd3329db58f2248ceaa5c5dcbd2f48
SimoSpark <mohamedoukhajou@gmail.com>
2025-03-04 21:08:14 +0000

    similar business

--- app/(routes)/details/_components/SuggestedBusinessList.jsx
+++ app/(routes)/details/_components/SuggestedBusinessList.jsx
@@ -1,6 +1,7 @@
 import GlobalApi from '@/app/_Services/GlobalApi';
 import { Button } from '@/components/ui/button'
 import { NotebookPen } from 'lucide-react'
+import Link from 'next/link';
 import React, { useEffect, useState } from 'react'
 function SuggestedBusinessList({business}) {
   
@@ -23,12 +24,30 @@ function SuggestedBusinessList({business}) {
                 <NotebookPen />
                 Book Appointment
             </Button>
-            <h2 className='font-bold 
-      text-lg mt-3 mb-3
-      
-      '>Similar Business</h2>
-        </div>
-    )
+            <h2 className='font-bold text-lg mt-3 mb-3'>Services Similaires</h2>
+            <div className=''>
+        {businessList&&businessList.map((business,index)=>(
+          <Link href={'/details/'+business.id} className="flex gap-2 mb-4
+          hover:border rounded-lg p-2
+          cursor-pointer hover:shadow-md
+           border-primary">
+            <Image src={business?.images[0].url}
+            alt={business.name}
+            width={80}
+            height={80}
+            className='rounded-lg object-cover h-[100px]'
+            />
+            <div className=''>
+              <h2 className='font-bold'>{business.name}</h2>
+              <h2 className='text-primary'>{business.contactPerson}</h2>
+              <h2 className='text-gray-400'>{business.address}</h2>
+
+            </div>
+          </Link>
+        ))}
+      </div>
+      </div>
+  )
 }
 
 export default SuggestedBusinessList
