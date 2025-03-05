a2bb415099a20b802bde61d894eaff6c7309ed36
SimoSpark <mohamedoukhajou@gmail.com>
2025-03-05 17:23:37 +0000

    adding time zones

--- app/(routes)/details/_components/BookingSection.jsx
+++ app/(routes)/details/_components/BookingSection.jsx
@@ -8,13 +8,17 @@ import {
     SheetTrigger,
 } from "@/components/ui/sheet"
 import { Calendar } from "@/components/ui/calendar"
+import { Button } from '@/components/ui/button';
 
-function BookingSection({ children }) {
-    const [date, setDate] = useState(null);
+function BookingSection({children}) {
+
+    const [date,setDate]=useState(new Date());
     const [timeSlot,setTimeSlot]=useState([]);
-    useEffect(() => {
-        setDate(new Date());
-    }, []);
+    useEffect(()=>{
+        getTime();
+       
+    },[])
+    
     const getTime = () => {
         const timeList = [];
         for (let i = 10; i <= 12; i++) {
@@ -37,40 +41,46 @@ function BookingSection({ children }) {
         setTimeSlot(timeList)
       }
 
-    return (
+    return  (
         <div>
+            
             <Sheet>
-                <SheetTrigger asChild>
-                    {children}
-                </SheetTrigger>
-                <SheetContent className="overflow-auto">
-                    <SheetHeader>
-                        <SheetTitle>Réserver le service</SheetTitle>
-                        <div>
-                        <SheetDescription>
-                            Sélectionnez la date et le créneau horaire pour réserver un service
-                            </SheetDescription>
-                        </div>
-
-                        <div className='flex flex-col gap-5 items-baseline'>
-                            <h2 className='mt-5 font-bold'>Select Date</h2>
-
-                            
-                            <div>
-                                {date && (
-                                    <Calendar
-                                        mode="single"
-                                        selected={date}
-                                        onSelect={setDate}
-                                        className="rounded-md border"
-                                    />
-                                )}
-                            </div>
-                        </div>
-
-                        <div>
-                            {/* time slot picker */}
-                        </div>
+      <SheetTrigger asChild>
+      {children}
+      </SheetTrigger>
+      <SheetContent className="overflow-auto">
+        <SheetHeader>
+          <SheetTitle>Book an Service</SheetTitle>
+          <SheetDescription>
+            Select Date and Time slot to book an service
+            {/* Date Picker  */}
+    
+            <div className='flex flex-col gap-5 items-baseline'>
+            <h2 className='mt-5 font-bold'>Select Date</h2>
+    
+                <Calendar
+                    mode="single"
+                    selected={date}
+                    onSelect={setDate}
+                    className="rounded-md border "
+                />
+    
+            </div>
+            {/* Time Slot Picker  */}
+            <h2 className='my-5 font-bold'>Select Time Slot</h2>
+            <div className='grid grid-cols-3 gap-3'>
+                {timeSlot.map((item,index)=>(
+                    <Button key={index}
+                    variant='outiline'
+                    className='border rounded-full 
+                    p-2 px-3 hover:bg-primary
+                     hover:text-white
+                    '
+                    >{item.time}</Button>
+                ))}
+            </div>
+            
+          </SheetDescription>
                     </SheetHeader>
                 </SheetContent>
             </Sheet>
